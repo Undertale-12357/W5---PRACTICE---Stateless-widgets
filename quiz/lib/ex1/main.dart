7d0154050f91ae4c6d055071d9ad744cb3bd6cb0
UndertaleXFrisk <eng.vireaksathya@gmail.com>
2025-11-13 09:16:34 +0700

    done

---
+++ quiz/lib/ex1/main.dart
@@ -0,0 +1,86 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(MyApp()); 
+}
+
+class HobbyCard extends StatelessWidget{
+  String title;
+  IconData iconData;
+  Color? color;
+
+  HobbyCard({super.key , required this.title , required this.iconData , this.color = Colors.blue});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(20),
+                        child: Icon(
+                          this.iconData,
+                          size: 30,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(
+                        this.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ), 
+              );
+  }
+}
+
+class MyApp extends StatelessWidget{
+  MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: _appBar(),
+        body: Padding(
+          padding: EdgeInsetsGeometry.all(50),
+          child: Column(
+            spacing: 20,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+                HobbyCard(title: "Travelling", iconData: Icons.travel_explore , color: Colors.green),
+                HobbyCard(title: "Skating", iconData: Icons.skateboarding , color: Colors.grey),
+                HobbyCard(title: "Booking", iconData: Icons.book , color: Colors.blue),
+            ]            
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+AppBar _appBar(){
+  return AppBar(
+    backgroundColor: Colors.white,
+    title: Text(
+      "My Hoobies",
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    )
+  );
+}
---
+++ quiz/lib/ex2/main.dart
@@ -0,0 +1,66 @@
+import "package:flutter/material.dart";
+
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Custom buttons"),
+          backgroundColor: Colors.white60,
+        ),
+        body: Center(
+          child: Column(
+              spacing: 20,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,  
+                  child: CustomButton(label: "Submit", icon: Icons.abc_sharp),
+                ),
+                SizedBox(
+                  width: double.infinity,  
+                  child: CustomButton(label: "Time", icon: Icons.time_to_leave_outlined , buttonType: "Secondary",)
+                ),
+                SizedBox(
+                  width: double.infinity,  
+                  child: CustomButton(label: "Turn In", icon: Icons.account_balance_wallet , buttonType: null,)
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget{
+  String label;
+  String? buttonType;
+  String? iconPosition;
+  IconData icon;
+
+  CustomButton({required this.label , required this.icon, super.key, this.buttonType = "primary" , this.iconPosition = "left"});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: buttonType == null? null : (){}, 
+      label: Text(label, style: TextStyle(fontSize: 30 , color: Colors.black , fontWeight: FontWeight.bold),),
+      icon: Icon(icon , size: 32),
+      iconAlignment: iconPosition == "left" ? IconAlignment.start : IconAlignment.end,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonType == "primary" ? Colors.blue : Colors.green,
+      ),
+    );
+  }
+  
+}
